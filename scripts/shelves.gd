6455a5fac1a90a540b97a4ca5e1402de79bd03cc
TAVI0 <marcos_tavio@outlook.com>
2025-02-17 03:53:55 -0300

    modif animacion

--- scripts/shelves.gd
+++ scripts/shelves.gd
@@ -2,14 +2,10 @@ extends DropZone
 
 var groupList = []
 var LONGSET = 3
-@onready var content = $Content
 @onready var markers = $Markers
-var marker_status: Dictionary = {}
 
 func _ready() -> void:
-	for marker in markers.get_children():
-		if marker is Marker2D:
-			marker_status[marker] = null
+	pass
 
 func addToGroupList(group):
 	groupList.append(group)
@@ -21,42 +17,33 @@ func checkSet():
 		completeSet()
 
 func completeSet():
-	for pot in content.get_children():
-		pot.queue_free()
+	for marker in markers.get_children():
+		var contentMarker = marker.get_child(0)
+		if contentMarker.get_child(0)!=null:
+			contentMarker.get_child(0).queue_free()
 	groupList.clear()
 	print(groupList)
 
 func add_decoration(decoration: Draggable):
-	decoration.reparent(content)
-	decoration.connect("dropped",
-		func(_dragged, _overlapping_areas):
-			reset_marker_status(_dragged)
-			find_a_place(_dragged)
-	)
-	find_a_place(decoration)
+	await find_a_place(decoration)
 	addToGroupList(decoration.group)
 
 func find_a_place(decoration):
 	var empty_marker = get_empty_marker()
 	if empty_marker != null:
-		move_to_marker(decoration, empty_marker)
+		await move_to_marker(decoration, empty_marker)
 		var content_marker=empty_marker.get_child(0)
 		decoration.reparent(content_marker)
 
 func move_to_marker(node: Node2D, marker: Marker2D):
-	get_tree().create_tween().tween_property(
+	var tween = get_tree().create_tween().tween_property(
 		node,
-		"position",
-		marker.position,
+		"global_position",
+		marker.global_position,
 		0.5
 	).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
-	marker_status[marker] = node
+	await tween.finished
 
-func reset_marker_status(decoration: Draggable):
-	for marker in marker_status.keys():
-		if marker_status[marker] == decoration:
-			marker_status[marker] = null
-			break
 
 func get_empty_marker():
 	for marker in markers.get_children():
